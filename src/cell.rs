776f292ba50f16a5a5462de7b96b66f643f5bb89
bruhman1269 <88716104+bruhman1269@users.noreply.github.com>
2024-01-26 23:12:40 -0800

    a

--- src/cell.rs
+++ src/cell.rs
@@ -1,6 +1,28 @@
 use macroquad::color::{self, colors, Color};
 use rand::Rng;
 use crate::{array2d::{Array2D, Error}, vector2::Vector2};
+pub struct CellData {
+    pub name: &'static str,
+    hue: f32,
+    saturation: f32,
+    lightness_range_lower: f32,
+    lightness_range_upper: f32,
+    falling_speed: f32,
+}
+
+impl CellData {
+    pub fn new(name: &'static str, hue: f32, saturation: f32, lr_lower: f32, lr_upper: f32, falling_speed: f32) -> Self {
+        return Self {
+            name,
+            hue,
+            saturation,
+            falling_speed,
+            lightness_range_lower: lr_lower,
+            lightness_range_upper: lr_upper
+        };
+    }
+}
+
 
 #[derive(Copy, Clone, Debug)]
 pub struct Cell {
@@ -8,19 +30,33 @@ pub struct Cell {
     pub position: Vector2<usize>,
     pub empty: bool,
     falling_speed: f32,
-    pub subposition: f32
+    subposition: f32,
+    should_fall_down: bool
 }
 
 impl Cell {
-    pub fn new(position: Vector2<usize>, falling_speed: f32) -> Self {
+
+    pub fn new(position: Vector2<usize>, hue: f32, saturation: f32, lr_lower: f32, lr_upper: f32, falling_speed: f32) -> Self {
         let mut rng = rand::thread_rng();
         return Self {
             position,
-            color: color::hsl_to_rgb(0.15, 0.48, rng.gen_range(0.57..0.78)),
+            color: color::hsl_to_rgb(hue, saturation, rng.gen_range(lr_lower..lr_upper)),
             falling_speed,
             empty: false,
-            subposition: 0.
+            subposition: 0.,
+            should_fall_down: falling_speed > 0.
         };
+    }
+
+    pub fn from_cell_data(cell_data: &CellData, position: Vector2<usize>) -> Self {
+        return Self::new(
+            position,
+            cell_data.hue,
+            cell_data.saturation,
+            cell_data.lightness_range_lower,
+            cell_data.lightness_range_upper,
+            cell_data.falling_speed
+        );
     }
 
     pub fn empty() -> Self {
@@ -29,7 +65,8 @@ impl Cell {
             position: Vector2::new(0, 0),
             empty: true,
             falling_speed: 0.,
-            subposition: 0.
+            subposition: 0.,
+            should_fall_down: false
         };
     }
 
@@ -51,28 +88,102 @@ impl Cell {
 
     pub fn simulate(&mut self, array_ref: &Array2D<Cell>, delta: f32) -> Option<Vector2<usize>> {
         let previous_position = self.position;
-        self.subposition += self.falling_speed * delta;
 
-        if self.subposition > 1. {
-            let subpos_floor = f32::floor(self.subposition);
-            let subpos_floor_usize = subpos_floor as usize;
-
-            if let Some(result) = self.raycast_down(array_ref, subpos_floor_usize) {
-                if let Ok(lower_cell) = result { 
-                    self.position = lower_cell.position.sub_vector(&Vector2::new(0usize, 1usize));
-                } else {
-                    self.position = Vector2::new(self.position.x, array_ref.size.y - 1usize);
-                }
-            } else {
-                self.position.add_self_vector(&Vector2::new(0usize, subpos_floor_usize));                
-            }
-            self.subposition -= subpos_floor;
+        if self.should_fall_down == true {
+            self.fall_down(array_ref, delta);
+        } else {
+            self.float_up(array_ref);
         }
 
         if previous_position != self.position {
             return Some(self.position);
         } else {
             return None
+        }
+    }
+
+    fn fall_down(&mut self, array_ref: &Array2D<Cell>, delta: f32) {
+        let lower_cell: &Cell;
+        if let Some(cell) = array_ref.get_from_vec(&self.position.add_vector(&Vector2::new(0usize, 1usize))) {
+            lower_cell = cell;
+        } else {
+            self.subposition = 0.;
+            return;
+        }
+        
+        self.subposition += self.falling_speed * delta;
+
+        if self.subposition > 1. {
+            let subpos_floor = f32::floor(self.subposition);
+            let subpos_floor_usize = subpos_floor as usize;
+
+            // If there's a cell directly below (try to fall to sides)
+            if lower_cell.empty == false {
+                let mut left_cell_empty = false;
+                let mut right_cell_empty = false;
+
+                if self.position.x > 0 {
+                    if let Some(left_cell) = array_ref.get_from_vec(&Vector2::new(self.position.x - 1usize, self.position.y + 1usize)) {
+                        left_cell_empty = left_cell.empty == true;
+                    }
+                }
+
+                if self.position.x < array_ref.size.x - 1usize {
+                    if let Some(right_cell) = array_ref.get_from_vec(&Vector2::new(self.position.x + 1, self.position.y + 1)) {
+                        right_cell_empty = right_cell.empty == true;
+                    }
+                }
+
+                if left_cell_empty && right_cell_empty {
+                    let go_left = rand::random::<bool>();
+                    if go_left == true {
+                        self.position.x -= 1usize;
+                    } else {
+                        self.position.x += 1usize;
+                    }
+                } else if left_cell_empty {
+                    self.position.x -= 1usize;
+                } else if right_cell_empty {
+                    self.position.x += 1usize;
+                }
+
+                if left_cell_empty || right_cell_empty {
+                    self.raycast_down_and_change_pos(array_ref, subpos_floor_usize);
+                }
+                
+            } else {
+                self.raycast_down_and_change_pos(array_ref, subpos_floor_usize);
+            }
+
+            self.subposition -= subpos_floor;
+        }
+    }
+
+    fn float_up(&mut self, array_ref: &Array2D<Cell>) {
+        if self.subposition < 1. {
+            let subpos_floor = f32::floor(self.subposition);
+            let subpos_floor_usize = subpos_floor as usize;
+
+            if let Some(result) = self.raycast_up(array_ref, subpos_floor_usize) {
+                if let Ok(upper_cell) = result {
+                    self.position = upper_cell.position.add_vector(&Vector2::new(0usize, 1usize));
+                } else {
+                    self.position.sub_self_vector(&Vector2::new(0usize, subpos_floor_usize))
+                }
+            }
+            self.subposition -= subpos_floor;
+        }
+    }
+
+    fn raycast_down_and_change_pos(&mut self, array_ref: &Array2D<Cell>, subpos_floor_usize: usize) {
+        if let Some(result) = self.raycast_down(array_ref, subpos_floor_usize) {
+            if let Ok(lower_cell) = result { 
+                self.position = lower_cell.position.sub_vector(&Vector2::new(0usize, 1usize));
+            } else {
+                self.position = Vector2::new(self.position.x, array_ref.size.y - 1usize);
+            }
+        } else {
+            self.position.add_self_vector(&Vector2::new(0usize, subpos_floor_usize));
         }
     }
 
@@ -84,6 +195,23 @@ impl Cell {
             ) {
                 if lower_cell.empty == false {
                     return Some(Ok(lower_cell));
+                }
+            } else {
+                return Some(Err(Error::IndexOutOfBounds));
+            }
+        }
+
+        return None;
+    }
+
+    fn raycast_up<'a>(&self, array_ref: &'a Array2D<Cell>, distance: usize) -> Option<Result<&'a Cell, Error>>{
+        for distance_index in 1..=distance {
+            //draw_rectangle((self.position.x * CELL_SIZE) as f32, ((self.position.y + distance_index + 1) * CELL_SIZE) as f32, CELL_SIZE as f32, CELL_SIZE as f32, RED);
+            if let Some(upper_cell) = array_ref.get_from_vec(
+                &self.position.sub_vector(&Vector2::new(0usize, distance_index))
+            ) {
+                if upper_cell.empty == false {
+                    return Some(Ok(upper_cell));
                 }
             } else {
                 return Some(Err(Error::IndexOutOfBounds));
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod array2d;
 
 use vector2::Vector2;
 use cell_grid::CellGrid;
-use cell::Cell;
+use cell::{Cell, CellData};
 use array2d::Array2D;
 
 const SCREEN_SIZE: Vector2<usize> = Vector2::new(600, 600);
@@ -15,12 +15,19 @@ const CELL_SIZE: usize = 5;
 
 #[macroquad::main("SandSim")]
 async fn main() {
+    let elements = [
+        CellData::new("Sand", 0.12, 0.38, 0.55, 0.68, 60.),
+        CellData::new("Gunpowder", 0.217, 0.08, 0.24, 0.38, 120.),
+        CellData::new("Dead Ladybugs", 0., 1., 0., 1., -25.)
+    ];
+
     // Create new array of cells
     let grid_size = SCREEN_SIZE.div_num(CELL_SIZE);
     let mut grid = CellGrid::new(grid_size, CELL_SIZE);
     request_new_screen_size(SCREEN_SIZE.x as f32, SCREEN_SIZE.y as f32 + 30.);
 
     let mut draw_size = Vector2::new(3, 3);
+    let mut current_element_index = 0;
 
     loop {
         let delta = get_frame_time();
@@ -35,7 +42,7 @@ async fn main() {
                 for y in 0..draw_size.y {
                     let position = mouse_pos.add_vector(&Vector2::new(x, y));
                     if position.x <= grid_size.x && position.y <= grid_size.y {
-                        let _ = grid.set_if_empty(&position, Cell::new(position, 40.));
+                        let _ = grid.set_if_empty(&position, Cell::from_cell_data(&elements[current_element_index], position));
                     }
                 }
             }
@@ -54,12 +61,26 @@ async fn main() {
                 draw_size.sub_self_vector(&Vector2::new(1usize, 1usize));
             }
         }
+
+        if is_key_pressed(KeyCode::Left) {
+            if current_element_index > 0 {
+                current_element_index -= 1;
+            }
+        }
+
+        if is_key_pressed(KeyCode::Right) {
+            if current_element_index < elements.len() - 1usize {
+                current_element_index += 1;
+            }
+        }
         
         grid.simulate(delta);
         grid.draw(true);
 
         draw_text(format!("FPS: {}", get_fps()).as_str(), 16., 48., 16., GREEN);
         draw_text(format!("Draw Size: {}", draw_size.x).as_str(), 16., 64., 16., GREEN);
+        draw_text(format!("Current Element: {}", elements[current_element_index].name).as_str(), 16., 80., 16., GREEN);
+
 
         
         draw_text("R to reset grid.", SCREEN_SIZE.x as f32 - 120., 16., 16., PINK);
